036fd5fecfd92aef76483e9cb9455a67b18f5a29
Sam Trenter <sntrenter@gmail.com>
2023-11-25 00:07:16 -0600

    stuff asdf

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # AccountRipper
+
+- so all dates rn are saved as central time, not sure if that's a problem or not
+- Need to add ability to rip story
+- need to check if reels are also ripped
+
+- twitter next
+
+- set up database
+
+2023-10-24_19-28-45_0
--- instagram.py
+++ instagram.py
@@ -5,6 +5,39 @@ from instaloader import Instaloader, Profile
 
 # Define the Instagram username
 username = "usedsoil"
+date = 1698140700
+
+def get_posts_till_date(username, date):
+    # Create an instance of Instaloader
+    loader = Instaloader()
+
+    # Load the profile of the Instagram account
+    profile = Profile.from_username(loader.context, username)
+
+    # Create a directory to save the downloaded photos
+    os.makedirs(username, exist_ok=True)
+
+    # Iterate over the posts of the profile
+    for post in profile.get_posts():
+
+        if post.date_utc.timestamp() < date:
+            print("Photos before the date have been downloaded")
+            break
+        # Iterate over the photos in the post
+        for index, photo in enumerate(post.get_sidecar_nodes()):
+            # Get the URL of the photo
+            url = photo.display_url
+
+            # Download the photo
+            response = requests.get(url)
+            if response.status_code == 200:
+                # Save the photo to the directory
+                with open(f"{username}/{post.date_utc.strftime('%Y-%m-%d_%H-%M-%S')}_{index}.jpg", "wb") as f:
+                    f.write(response.content)
+                print(f"Downloaded photo: {post.date_utc.strftime('%Y-%m-%d_%H-%M-%S')}_{index}.jpg")
+
+    print("All photos downloaded successfully!")
+
 
 def get_latest_post_timestamp(username):
     # Create an instance of Instaloader
@@ -56,6 +89,7 @@ if __name__ == "__main__":
     # Get the latest post timestamp
     timestamp = get_latest_post_timestamp(username)
     print(f"Latest post timestamp: {timestamp}")
+    get_posts_till_date(username, date)
 
     # Download all the photos
     #Download_all_photos(username)
